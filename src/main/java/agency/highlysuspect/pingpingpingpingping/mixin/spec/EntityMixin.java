3df1b5b7da5c30071ba12ea4311dc2c85f7bf5dd
quat1024 <quat1024@users.noreply.github.com>
2023-08-23 19:43:53 -0400

    fix spectator stuff

--- src/main/java/agency/highlysuspect/pingpingpingpingping/mixin/spec/EntityMixin.java
+++ src/main/java/agency/highlysuspect/pingpingpingpingping/mixin/spec/EntityMixin.java
@@ -10,8 +10,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class EntityMixin {
-	@Inject(method = "isInvisibleTo", at = @At("HEAD"), cancellable = true)
+	@Inject(method = "isInvisibleTo", at = @At("RETURN"), cancellable = true)
 	private void ping5$isInvisibleTo(Player player, CallbackInfoReturnable<Boolean> cir) {
-		if(PingPingPingPingPing.HIDE_SPECTATORS && player.isSpectator()) cir.setReturnValue(true);
+		if(PingPingPingPingPing.HIDE_SPECTATORS && player.isSpectator()) {
+			//isInvisibleTo is called in two places in LivingEntityRenderer, both times
+			//passing Minecraft.getInstance().player as the argument.
+			//the first is in LivingEntityRenderer#render - if the entity isInvisible but
+			//this method returns *false*, the semitransparent rendering mode will be used.
+			//I would like to avoid that so i will return 'true'. This is how things like
+			//invisible armor stands get rendered in spectator mode (by default this method
+			//always returns 'false' for spectating players).
+			//The second place is in shouldShowName; returning 'true' will hide the nametag.
+			
+			//If I was smart I'd modify those calls to do different things. Im not smart tho
+			//and the mixins look annoying. So i return true for all non-spectator players and
+			//another mixin takes care of removing the models of spectator-mode players
+			
+			Entity other = (Entity) (Object) this;
+			if(other instanceof Player) {
+				if(other.isSpectator()) cir.setReturnValue(true);
+			} else {
+				cir.setReturnValue(true);
+			}
+		}
 	}
 }
